8bad83cae82f94730123bcbb7d5a443433f66a22
meryem.sekkat <meryem.sekkat@alithya.com>
2022-08-10 17:04:43 +0100

    fix: item change pagination

--- backend/route/posts.js
+++ backend/route/posts.js
@@ -71,16 +71,22 @@ router.get('' , (req, res, next) => {
   const pageSize = +req.query.pageSize;
   const currentPage = +req.query.page;
   const postQuery = Post.find();
+  let fetchedPosts;
   if(pageSize && currentPage) {
     postQuery
       .skip(pageSize * (currentPage - 1))
       .limit(pageSize)
   }
-  postQuery.find()
+  postQuery
     .then(documents => {
+      fetchedPosts = documents
+      return Post.count();
+    })
+    .then(count => {
       res.status(200).json({
         message: 'post fetched successfly',
-        posts: documents
+        posts: fetchedPosts,
+        maxPost: count
       })
     })
     .catch();
