eccb14fe2a0992c9bab3849b09dd02ed089c9483
meryem.sekkat <meryem.sekkat@alithya.com>
2022-08-10 11:58:34 +0100

    uploading image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -38,8 +38,9 @@ export class PostCreateComponent implements OnInit {
 
 ngOnInit(): void {
   this.form = new FormGroup({
-    'title': new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
-    'content': new FormControl(null, {validators: Validators.required} )
+    title: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
+    content: new FormControl(null, {validators: Validators.required} ),
+    image: new FormControl(null, {validators: [Validators.required]})
   })
   this.route.paramMap.subscribe((paramsMap) => {
     if(paramsMap.has('id')) {
@@ -80,4 +81,12 @@ ngOnInit(): void {
     this.form.reset();
   }
 
+  onImagePicked(event: Event) {
+    // @ts-ignore
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file})
+    this.form.get('image')?.updateValueAndValidity();
+    console.log(file);
+    console.log(this.form)
+  }
 }
